b62d0ae57f5621d0580a48cc3c686bcee90810f1
Muhammad Bilal <muhdbilal1996@gmail.com>
2024-03-03 08:11:37 +0500

    new project created also connected with firebase

--- App.tsx
+++ App.tsx
@@ -8,16 +8,8 @@ import database from '@react-native-firebase/database';
 
 
 const App = () =>{
-  useEffect(()=>{
-    database()
-    .ref('/users')
-    .set({
-      name: 'Ada Lovelace',
-      age: 31,
-    })
-    .then(() => console.log('Data set.'));
 
-  },[])
+
 
   return (
         <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
