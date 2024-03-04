f09dc8fa86f5fe56977509fdf4b03136216a92a4
Muhammad Bilal <muhdbilal1996@gmail.com>
2024-03-04 08:41:22 +0500

    navigation added also login signup flow completed and code mereged into main

--- App.tsx
+++ App.tsx
@@ -17,6 +17,7 @@ function App() {
         screenOptions={{
           headerShown: false,
       }}>
+        
         <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
         <Stack.Screen  name="SignInScreen" component={SignInScreen} />
         <Stack.Screen name="HomeScreen" component={HomeScreen} />
