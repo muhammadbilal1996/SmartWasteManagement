976f84fe16d7b1bebd74f2f63c5b4971c9aad12d
shahid <muhammadshahidaslam73@gmail.com>
2024-03-12 00:49:23 -0700

    set profile screen

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -1,45 +1,53 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, Platform, PermissionsAndroid ,TouchableHighlight} from "react-native";
-import Header from "../../components/Header";
-import LinearGradient from "react-native-linear-gradient";
-import {Colors} from "../../utills/Colors";
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Platform,
+  PermissionsAndroid,
+  TouchableHighlight,
+} from 'react-native';
+import Header from '../../components/Header';
+import LinearGradient from 'react-native-linear-gradient';
+import {Colors} from '../../utills/Colors';
 import * as ImagePicker from 'react-native-image-picker';
 import IconImage from 'react-native-vector-icons/FontAwesome';
 import Modal from 'react-native-modal';
 
 const ProfileScreen = () => {
-    const [name, setName] = useState('');
-    const [password, setPassword] = useState('');
-    const [image, setImage] = useState(null);
+  const [name, setName] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [image, setImage] = useState(null);
 
-    const [circleImage, setCircleImage] = useState('');
-  
-    const [ImageModal, setImageModal] = useState(false);
-  
-    const [SeletecImageForSend, setSeletecImageForSend] = useState([]);
+  const [circleImage, setCircleImage] = useState('');
 
-    const handleNameChange = (text) => {
-        setName(text);
-    }
+  const [ImageModal, setImageModal] = useState(false);
 
-    const handlePasswordChange = (text) => {
-        setPassword(text);
-    }
+  const [SeletecImageForSend, setSeletecImageForSend] = useState([]);
 
-    const handleImageUpload = () => {
-        // Implement image upload logic here
-        // This can involve using libraries like react-native-image-picker or react-native-camera
-    }
+  const handleNameChange = text => {
+    setName(text);
+  };
 
-    const handleUpdateProfile = () => {
-        // Implement profile update logic here
-        console.log("Name:", name);
-        console.log("Password:", password);
-        console.log("Image:", image);
-    }
+  const handlePasswordChange = text => {
+    setPassword(text);
+  };
 
+  const handleImageUpload = () => {
+    // Implement image upload logic here
+    // This can involve using libraries like react-native-image-picker or react-native-camera
+  };
 
-      // openGallery function will load image from gallery
+  const handleUpdateProfile = () => {
+    const data = {name: name, image: circleImage, contact: phoneNumber};
+    console.log(data);
+  };
+
+  // openGallery function will load image from gallery
   function openGallery() {
     let options = {
       storageOptions: {
@@ -58,12 +66,18 @@ const ProfileScreen = () => {
         let file = {
           name: new Date() + '.jpg',
           type: 'image/jpeg',
-          uri: Platform.OS === 'android' ? response.assets[0].uri : response.assets[0].uri.replace('file://', ''),
+          uri:
+            Platform.OS === 'android'
+              ? response.assets[0].uri
+              : response.assets[0].uri.replace('file://', ''),
         };
         let image = {
           name: new Date() + '.jpg',
           type: 'image/jpeg',
-          uri: Platform.OS === 'android' ? response.assets[0].uri : response.assets[0].uri.replace('file://', ''),
+          uri:
+            Platform.OS === 'android'
+              ? response.assets[0].uri
+              : response.assets[0].uri.replace('file://', ''),
         };
         setImageModal(false);
         setCircleImage(image.uri);
@@ -73,18 +87,21 @@ const ProfileScreen = () => {
   }
 
   const requestCameraPermission = async () => {
-    console.log("Camera press..........................");
+    console.log('Camera press..........................');
     try {
       if (Platform.OS == 'ios') {
         openCamera();
       } else {
-        const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.CAMERA, {
-          title: 'App Camera Permission',
-          message: 'App needs access to your camera ',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        });
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          {
+            title: 'App Camera Permission',
+            message: 'App needs access to your camera ',
+            buttonNeutral: 'Ask Me Later',
+            buttonNegative: 'Cancel',
+            buttonPositive: 'OK',
+          },
+        );
         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
           // Permission Granted
           openCamera();
@@ -120,12 +137,18 @@ const ProfileScreen = () => {
         let file = {
           name: new Date() + '.jpg',
           type: 'image/jpeg',
-          uri: Platform.OS === 'android' ? response.assets[0].uri : response.assets[0].uri.replace('file://', ''),
+          uri:
+            Platform.OS === 'android'
+              ? response.assets[0].uri
+              : response.assets[0].uri.replace('file://', ''),
         };
         let image = {
           name: new Date() + '.jpg',
           type: 'image/jpeg',
-          uri: Platform.OS === 'android' ? response.assets[0].uri : response.assets[0].uri.replace('file://', ''),
+          uri:
+            Platform.OS === 'android'
+              ? response.assets[0].uri
+              : response.assets[0].uri.replace('file://', ''),
         };
         setImageModal(false);
         setCircleImage(image.uri);
@@ -134,21 +157,23 @@ const ProfileScreen = () => {
     });
   }
 
-  
   // this renderUserPictureView function will display user picture and its design
   const renderUserPictureView = () => {
     return (
       <View>
         <View
           style={{
-            ...styles.UserImageViews
+            ...styles.UserImageViews,
           }}>
-          <TouchableOpacity onPress={() => setImageModal(true)} activeOpacity={0.95} style={{}}>
+          <TouchableOpacity
+            onPress={() => setImageModal(true)}
+            activeOpacity={0.95}
+            style={{}}>
             {!circleImage ? (
               <View
                 style={{
                   ...styles.ImageUser,
-                  marginTop:8,
+                  marginTop: 8,
                   borderWidth: 3,
                   borderColor: '#ffffff',
                   justifyContent: 'center',
@@ -165,12 +190,11 @@ const ProfileScreen = () => {
               </View>
             ) : (
               <View style={styles.CicrleSelectImage}>
-                 <Image
+                <Image
                   resizeMode={'contain'}
-                  source={{uri :circleImage}}
+                  source={{uri: circleImage}}
                   style={styles.ImageUser}
                 />
-               
               </View>
             )}
           </TouchableOpacity>
@@ -181,7 +205,6 @@ const ProfileScreen = () => {
             style={{
               ...styles.btnViewpro,
             }}>
-
             <Text style={styles.btnTextpro}>Upload Photo</Text>
           </TouchableOpacity>
         </View>
@@ -189,160 +212,167 @@ const ProfileScreen = () => {
     );
   };
 
-    return (
-        <View style={styles.mainContainer}>
-            <Header title={'Profile'} />
-            {renderUserPictureView()}
-            <View style={styles.profileContainer}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Name"
-                    onChangeText={handleNameChange}
-                    value={name}
-                />
-              
-            
-                <TouchableOpacity
-                    onPress={handleUpdateProfile}
-                    style={[
-                        styles.signIn,
-                        {
-                            borderColor: Colors.primary_dark,
-                            borderWidth: 1,
-                            marginTop: 15,
-                        },
-                    ]}>
-                    <LinearGradient
-                        colors={[Colors.primary, Colors.primary_dark]}
-                        style={styles.signIn}>
-                        <Text style={[styles.textSign, { color: '#fff' }]}>Update</Text>
-                    </LinearGradient>
-                </TouchableOpacity>
+  return (
+    <View style={styles.mainContainer}>
+      <Header title={'Profile'} />
+      {renderUserPictureView()}
+      <View style={styles.profileContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Name"
+          onChangeText={handleNameChange}
+          value={name}
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Phone number"
+          onChangeText={setPhoneNumber}
+          value={phoneNumber}
+          keyboardType={'numeric'}
+        />
+
+        <TouchableOpacity
+          onPress={handleUpdateProfile}
+          style={[
+            styles.signIn,
+            {
+              borderColor: Colors.primary_dark,
+              borderWidth: 1,
+              marginTop: 15,
+            },
+          ]}>
+          <LinearGradient
+            colors={[Colors.primary, Colors.primary_dark]}
+            style={styles.signIn}>
+            <Text style={[styles.textSign, {color: '#fff'}]}>Update</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+      {/* picture modal */}
+
+      <Modal
+        style={styles.ModalViewPic}
+        onBackButtonPress={() => setImageModal(false)}
+        onBackdropPress={() => setImageModal(false)}
+        visible={ImageModal}
+        transparent
+        animationType="slide"
+        onRequestClose={() => setImageModal(false)}
+        hardwareAccelerated>
+        <View style={styles.ModalView}>
+          <TouchableHighlight
+            underlayColor={'#ffffff'}
+            onPress={() => requestCameraPermission()}
+            style={{
+              borderBottomColor: 'black',
+              borderBottomWidth: 1,
+            }}>
+            <View style={styles.modalItem}>
+              <IconImage name="camera" size={25} style={{color: 'black'}} />
+              <Text style={styles.modalItemText}>Camera</Text>
             </View>
-               {/* picture modal */}
+          </TouchableHighlight>
 
-          <Modal
-            style={styles.ModalViewPic}
-            onBackButtonPress={() => setImageModal(false)}
-            onBackdropPress={() => setImageModal(false)}
-            visible={ImageModal}
-            transparent
-            animationType="slide"
-            onRequestClose={() => setImageModal(false)}
-            hardwareAccelerated>
-            <View style={styles.ModalView}>
-              <TouchableHighlight
-                underlayColor={'#ffffff'}
-                onPress={() => requestCameraPermission()}
-                style={{
-                  borderBottomColor: 'black',
-                  borderBottomWidth: 1,
-                }}>
-                <View style={styles.modalItem}>
-                  <IconImage name="camera" size={25} style={{color: 'black'}} />
-                  <Text style={styles.modalItemText}>Camera</Text>
-                </View>
-              </TouchableHighlight>
-
-              <TouchableHighlight
-                underlayColor="#ddd"
-                style={{
-                  borderBottomColor: 'black',
-                  borderBottomWidth: 1,
-                }}
-                onPress={() => openGallery()}>
-                <View style={styles.modalItem}>
-                  <IconImage name="photo" size={25} style={{color: 'black'}} />
-                  <Text style={styles.modalItemText}>Gallery</Text>
-                </View>
-              </TouchableHighlight>
-
-              <TouchableHighlight
-                underlayColor={'#ffffff'}
-                style={{
-                  borderBottomColor: 'transparent',
-                  borderBottomWidth: 1,
-                  marginLeft: 4,
-                }}
-                onPress={() => setImageModal(false)}>
-                <View style={styles.modalItem}>
-                  <IconImage name="close" size={25} style={{color: 'red'}} />
-                  <Text style={styles.modalItemText}>Cancel</Text>
-                </View>
-              </TouchableHighlight>
+          <TouchableHighlight
+            underlayColor="#ddd"
+            style={{
+              borderBottomColor: 'black',
+              borderBottomWidth: 1,
+            }}
+            onPress={() => openGallery()}>
+            <View style={styles.modalItem}>
+              <IconImage name="photo" size={25} style={{color: 'black'}} />
+              <Text style={styles.modalItemText}>Gallery</Text>
             </View>
-          </Modal>
+          </TouchableHighlight>
+
+          <TouchableHighlight
+            underlayColor={'#ffffff'}
+            style={{
+              borderBottomColor: 'transparent',
+              borderBottomWidth: 1,
+              marginLeft: 4,
+            }}
+            onPress={() => setImageModal(false)}>
+            <View style={styles.modalItem}>
+              <IconImage name="close" size={25} style={{color: 'red'}} />
+              <Text style={styles.modalItemText}>Cancel</Text>
+            </View>
+          </TouchableHighlight>
         </View>
-    )
-}
+      </Modal>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    mainContainer: {
-        flex: 1,
-        marginHorizontal: 12
-    },
-    profileContainer: {
-        alignItems: 'center',
-        marginTop: 20,
-    },
-    profileImage: {
-        width: 100,
-        height: 100,
-        borderRadius: 50,
-    },
-    btnViewpro: {
-        height: 38,
-        width: '45%',
-        marginVertical:20,
-        alignContent: 'center',
-        alignSelf: 'center',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: Colors.primary,
-        borderRadius: 27,
-      },
-      btnTextpro: {
-        color: Colors.white,
-        justifyContent: 'center',
-        alignSelf: 'center',
-        letterSpacing: 0.04,
-        fontSize: 12,
-      },
-    input: {
-        height: 48,
-        width: '100%',
-        margin: 10,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        padding: 10,
-        borderRadius: 10,
-    },
-    updateButton: {
-        backgroundColor: 'gray',
-        paddingHorizontal: 50,
-        paddingVertical: 8,
-        borderRadius: 5,
-        marginTop: 20,
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 16,
-        textAlign: 'center',
-    },
-    signIn: {
-        width: '100%',
-        height: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-    },
-    textSign: {
-        fontSize: 18,
-        fontWeight: 'bold',
-    },
-   //////////////////////////////////////////////////////////
-  
+  mainContainer: {
+    flex: 1,
+    marginHorizontal: 12,
+  },
+  profileContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+  },
+  btnViewpro: {
+    height: 38,
+    width: '45%',
+    marginVertical: 20,
+    alignContent: 'center',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.primary,
+    borderRadius: 27,
+  },
+  btnTextpro: {
+    color: Colors.white,
+    justifyContent: 'center',
+    alignSelf: 'center',
+    letterSpacing: 0.04,
+    fontSize: 12,
+  },
+  input: {
+    height: 48,
+    width: '100%',
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 10,
+  },
+  updateButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 50,
+    paddingVertical: 8,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  //////////////////////////////////////////////////////////
+
   CicrleSelectImage: {
     height: 100,
     width: 100,
@@ -354,7 +384,6 @@ const styles = StyleSheet.create({
     borderColor: Colors.white,
     alignItems: 'center',
   },
- 
 
   Title: {
     flex: 1,
@@ -368,14 +397,13 @@ const styles = StyleSheet.create({
 
   UserImageViews: {
     marginTop: 5,
-    alignItems:'center'
+    alignItems: 'center',
   },
 
   ModalViewPic: {
     justifyContent: 'flex-end',
     margin: 0,
     backgroundColor: 'rgba(0,0,0,0.5)',
-
   },
   ImageUser: {
     height: 100,
@@ -384,16 +412,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignSelf: 'center',
   },
- 
 
- 
   heading: {
     fontSize: 32,
     fontWeight: 'bold',
     color: Colors.black,
   },
-
-
 
   modalItemText: {
     marginLeft: 14,
@@ -412,10 +436,8 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     borderTopLeftRadius: 20,
     paddingVertical: 20,
-    backgroundColor:'#ffffff'
+    backgroundColor: '#ffffff',
   },
-
-
 
   //Modal view
   modalView: {
@@ -425,11 +447,6 @@ const styles = StyleSheet.create({
     margin: -20,
     backgroundColor: 'rgba(35,36,65,0.6)',
   },
-
-
-
-
-
 });
 
 export default ProfileScreen;
